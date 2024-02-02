561fcd44f4b9c1aff2c87746c12d34ad5eee2c56
ADXID12 <131492273+ADXID12@users.noreply.github.com>
2024-02-02 16:10:26 -0600

    Add files via upload

--- Lab3P2_GabrielOsorto/Lab3P2_GabrielOsorto/src/lab3p2_gabrielosorto/Lab3P2_GabrielOsorto.java
+++ Lab3P2_GabrielOsorto/Lab3P2_GabrielOsorto/src/lab3p2_gabrielosorto/Lab3P2_GabrielOsorto.java
@@ -10,34 +10,42 @@ public class Lab3P2_GabrielOsorto {
     static ArrayList<Pokeball> pokebolas = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Menu PokeHot");
-        System.out.println("1. Crear Pokemon");
-        System.out.println("2. Crear Pokebola");
-        System.out.println("3. Listar Pokemon");
-        System.out.println("4. Eliminar Pokemon");
-        System.out.println("5. Capturar Pokemon");
-        System.out.println("6. Modificar Pokemon");
-        System.out.println("7. Salir");
-        System.out.print("Ingrese su opcion:");
-        int op = sc.nextInt();
-        switch (op) {
-            case 1:
-                Case1();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            default:
-                break;
+        boolean seguir = true;
+        while (seguir == true) {
+            System.out.println("Menu PokeHot");
+            System.out.println("1. Crear Pokemon");
+            System.out.println("2. Crear Pokebola");
+            System.out.println("3. Listar Pokemon");
+            System.out.println("4. Eliminar Pokemon");
+            System.out.println("5. Capturar Pokemon");
+            System.out.println("6. Modificar Pokemon");
+            System.out.println("7. Salir");
+            System.out.print("Ingrese su opcion:");
+            int op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    Case1();
+                    break;
+                case 2:
+                    Case2();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.out.println("Saliendo.....");
+                    seguir = false;
+                    break;
+                default:
+                    System.out.println("OPCION NO VALIDA");
+                    System.out.println("");
+                    break;
+            }
         }
     }
 
@@ -87,8 +95,27 @@ public class Lab3P2_GabrielOsorto {
                 FireType nuevoFirePokemon = new FireType(nombrePok, EntradaPoke, NatuPoke, atrapado, null, potenLlama);
                 pokes.add(nuevoFirePokemon);
                 break;
-
         }
+        System.out.println("Pokemon creado de forma exitosa :D");
+        System.out.println("");
     }
 
+    public static void Case2() {
+        System.out.print("Ingrese el color de la pokebola:");
+        String ColorPokeball = lt.nextLine();
+        System.out.print("Ingrese el numero de serie de la Pokebola:");
+        int SeriePokeball = sc.nextInt();
+        System.out.print("Ingrese la eficiencia para atrapar Pokehots(1-3):");
+        int RangoEfi = sc.nextInt();
+        while (RangoEfi < 0 || RangoEfi > 3) {
+            System.out.print("Ingrese un rango valido:");
+            RangoEfi = sc.nextInt();
+        }
+        Pokeball nuevaPokeball = new Pokeball(ColorPokeball, SeriePokeball, RangoEfi);
+        pokebolas.add(nuevaPokeball);
+    }
+
+    public static void Case3() {
+
+    }
 }
